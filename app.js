ed38db86fab5f1cd3b6d3ba006c117cbfcd92458
pfrimpong11 <frimpongprince2000@gmail.com>
2024-10-11 22:11:15 +0000

    update

--- app.js
+++ app.js
@@ -42,4 +42,4 @@ app.use('/api', require('./routes/adminRoutes'));
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
